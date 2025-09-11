fbc6d582caefb1501bafe998defb2e157ff40d49
prahlad patidar <152579920+prahladpatidar@users.noreply.github.com>
2025-09-11 10:46:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Jai shree Ram 
+
+const max=prompt("Enter your maximum number");
+const randomnum=Math.floor(Math.random()*max)+1;
+
+let guess=prompt("guess your number")
+
+while(true){
+    if(guess=="quit"){
+        console.log("user quit")
+        break;
+    } 
+     if(randomnum==guess){
+        console.log("congrat your right guess random number was",randomnum);
+        break;
+    }else if(guess<randomnum){
+        guess=prompt("your guess was too small. plaese try again ");
+    } else {
+         guess=prompt("your guess was too large. plaese try again ");
+    }
+
+    
+}
